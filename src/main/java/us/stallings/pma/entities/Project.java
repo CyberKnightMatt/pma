47137e3ebb1378b918c6e3e388a3f9603f13c39a
Matt <matt@stallings.us>
2021-10-10 16:48:24 -0500

    Implement command line runner to load test data.

--- src/main/java/us/stallings/pma/entities/Project.java
+++ src/main/java/us/stallings/pma/entities/Project.java
@@ -1,6 +1,7 @@
 package us.stallings.pma.entities;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -69,5 +70,12 @@ public class Project {
 
     public void setEmployees(List<Employee> employees) {
         this.employees = employees;
+    }
+
+    public void addEmployee(Employee employee) {
+        if(employees==null) {
+            employees = new ArrayList<>();
+        }
+        employees.add(employee);
     }
 }
